53793c9aa8efeef7f21baae67e47688d161a5d11
AinsonZ <ainsonzou@gmail.com>
2024-04-30 22:33:35 -0400

    MC documentation

--- MC.java
+++ MC.java
@@ -23,6 +23,13 @@ public class MC extends Prisoner
     private int bannerPosX;
     private int bannerPosY;
     // For cutscene/intro world
+    /**
+     * MC constructor
+     * 
+     * @param i
+     * @param inIntro                       Checks if the MC is in the intro world
+     * @param specialty                     Sets specialty of MC
+    */
     public MC(int i, boolean inIntro, String specialty){
         super(i, inIntro);
         this.specialty = specialty;
@@ -34,13 +41,11 @@ public class MC extends Prisoner
     }
 
     /**
-     * Prisoner constructor
+     * MC constructor
      * 
-     * @param name                      name of the character
-     * @param initialStrength           sets an initial strength stat
-     * @param initialSpeed              sets an initial speed stat
-     * @param initialLuck               sets an initial luck stat
-     * @param initialIntelligence       sets an initial intelligence stat
+     * @param i                         
+     * @param world                     Which world to place the MC in
+     * @param serializedData            takes in name, job, str, speed, intel and specialty
      */
     public MC(int i, MyWorld world, String[] serializedData) {
         super(i, serializedData[1]);
@@ -56,42 +61,78 @@ public class MC extends Prisoner
     }
 
     /**
-     * Act - do whatever the MC wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Act method of MC
      */
     public void act()
     {
         super.act();
     }
-
+    
+    /**
+     * Checks if MC is done crafting
+     * 
+     * @return doneCraft                Boolean to see if crafting is finished
+     */
     public boolean isDoneCrafting() {
         return doneCrafting;
     }
 
+    /**
+     * 
+     * @param isCrafting
+     */
     public void setDoneCrafting(boolean isCrafting) {
         this.doneCrafting = isCrafting;
     }
     
+    /**
+     * Gets the escape method MC is performing
+     * 
+     * @return escapeMethod             Gets the escape method of the MC
+     */
     public String getEscapeMethod(){
         return escapeMethod;
     }
     
+    /**
+     * 
+     */
     public void setEscapeMethod(String method){
         escapeMethod = method;
     }
 
+    /**
+     * Gets the specialty of MC
+     * 
+     * @return specialty                Gets the MC's specialty
+     */
     public String getSpecialty() {
         return specialty;
     }
 
+    /**
+     * Gets the name of MC
+     * 
+     * @return name                     Gets the MC's name
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * Gets the MC's current action
+     * 
+     * @return currentAction            Gets the MC's current action
+     */
     public String getCurrentAction(){
         return currentAction;
     }
-
+    
+    /**
+     * Sets status of MC's on game menu bar
+     * 
+     * @param action                    Action of MC to update status bar
+     */
     public void setAction(String action) {
         if(isDead) {
             this.currentAction = "Reviving...";
@@ -105,6 +146,11 @@ public class MC extends Prisoner
         }
     }
 
+    /**
+     * Gives item to MC
+     * 
+     * @param item                      Item that is given to MC
+     */
     public void giveItem(Item item) {
         for(Item i : heldItems) {
             if(i.getClass() == item.getClass()) {
@@ -115,14 +161,11 @@ public class MC extends Prisoner
         item.pickup(this);
         this.heldItems.add(item);
     }
-
-    // public void removeItem(Item item) {
-    // this.heldItems.remove(item);
-    // }
+    
     /**
-     * This method will craft the next material item in heldItems
+     * This method will                 craft the next material item in heldItems
      * 
-     * @return boolean returns true if there are no more materials in heldItems
+     * @return boolean                  returns true if there are no more materials in heldItems
      */
     public boolean craftItem() {
         for(Item i : heldItems) {
@@ -137,21 +180,35 @@ public class MC extends Prisoner
         return true;
     }
 
+    /**
+     * Returns the held items of MC
+     * 
+     * @return heldItems                Array of MC's held items
+     */
     public ArrayList<Item> getItems() {
         return heldItems;
     }
 
+    /**
+     * Tracks which index the MC is
+     * 
+     * @return index                    Index of MC in the world
+     */
     public int getIndex(){
         return index;
     }
 
+    /**
+     * Gets the accessories and assigns based on MC's specialty
+     * 
+     * @return result                   Array of accessories given to MC
+     */
     public ArrayList<Integer> getAccessories(){
         ArrayList<Integer> result = new ArrayList<>();
         if(specialty.equals("Scientist")){ 
             result.add(1);
         }
         else if(specialty.equals("Thief")){
-
             result.add(8);
             result.add(10);
         }
@@ -171,6 +228,11 @@ public class MC extends Prisoner
         return result; 
     }
 
+    /**
+     * Adds item to MC item slot
+     * 
+     * @param item                      Item given to MC
+     */
     public void addItem(Item item) {
         for (int i = 0; i < items.length; i++) {
             if (items[i] == null) {  // Check if the slot is empty
@@ -182,6 +244,11 @@ public class MC extends Prisoner
         System.out.println("Inventory is full. Cannot add item.");
     }
 
+    /**
+     * Returns total count of items
+     * 
+     * @return count                    Counts amount of items
+     */
     public int getItemCount() {
         int count = 0;
         for (Item item : items) {
@@ -192,6 +259,13 @@ public class MC extends Prisoner
         return count;  // Return the total count of items
     }
 
+    /**
+     * Creates a dialogue of what the MC says
+     * 
+     * @param text                      The text of the MC
+     * @param seconds                   Determines how long the banner should stay on screen
+     * @param speakerName               Name of the MC dialogue is spoken by
+     */
     public void dialogue(String text, int seconds, String speakerName){
 
         world.addObject(new Dialogue(text,seconds,speakerName),bannerPosX,bannerPosY); 
